bf945f8996f4e17dcd9efa5ee349d8f19033db58
lee-sunkyoung <ardcxvsk@gmail.com>
2026-04-28 17:48:20 +0900

    2

--- build/serial_joy_receiver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jaewoong/go2_ws/src/serial_joy_receiver
-# Build directory: /home/jaewoong/go2_ws/src/serial_joy_receiver/build/serial_joy_receiver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
